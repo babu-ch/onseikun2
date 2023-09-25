386c1e65dfcb73002e61a05cef7999b75b31bc13
babu-ch <mashirohira@gmail.com>
2023-09-25 18:54:33 +0900

    store

---
+++ src/components/Assistant.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="assistantBox">
+    <button>🎤</button>
+    <p>msg</p>
+    <p>msg</p>
+    <p>msg</p>
+    <p>msg</p>
+    <p>msg</p>
+    <p>msg</p>
+    <p>msg</p>
+    <p>msg</p>
+  </div>
+</template>
+
+<style scoped>
+.assistantBox {
+  right: 0;
+  bottom: 0;
+  position: fixed;
+  height: 400px;
+  width: 300px;
+  background: #213547;
+  padding: 10px;
+}
+.assistantBox p {
+  border-bottom: 1px solid #ccc;
+}
+</style>
